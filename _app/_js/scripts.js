01e0aede057819b9c58d14cde2c1122f3fb68b2c
PavelRoR <pavelvp2182@yandex.ru>
2021-01-13 20:59:03 +0500

    main page is adapted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,5 +1,6 @@
 //@prepros-prepend jquery-2.1.1.min.js
 //@prepros-prepend jquery.fancybox.min.js
+//@prepros-prepend slick.min.js
 
 $(document).ready(function() {
     $(function () {
@@ -55,7 +56,78 @@ $(document).ready(function() {
         });
     });
     $('.button-up').fancybox();
-
+    $('[data-fancybox]').fancybox({
+        loop: 1,
+    });
+    $('.revs-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        appendArrows: '.video-revs-arrows-1',
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        appendArrows: '.video-revs-arrows-2',
+        responsive: [
+        {
+            breakpoint: 561,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.slick-dots').wrap('<div class="container container-revs">')
+    /* Видео */
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&modestbranding=1&mute=1&showinfo=0&rel=0" allowfullscreen></iframe>')
+    });
+    function imToVideo(){
+        $('.video-wrapper-revs iframe').each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+        });
+        $(".video-wrapper-revs img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?modestbrandig=1&mute=1&showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>');
+        });
+    }
+$('.revs-slider').on('swipe', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('afterChange', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('beforeChange', function (event, slick, direction) {
+    imToVideo();
+});
 
 /*Конец документа*/
 });
