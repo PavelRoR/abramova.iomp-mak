174a3bab7d7436c752755c5cf42b73ed387423c4
PavelRoR <pavelvp2182@yandex.ru>
2021-01-15 14:50:17 +0500

    course page is typeseted

--- _app/_js/course.js
+++ _app/_js/course.js
@@ -1,7 +1,37 @@
 //@prepros-prepend jquery-2.1.1.min.js
 //@prepros-prepend jquery.fancybox.min.js
 //@prepros-prepend slick.min.js
+function Search(el) {
+	return document.querySelector(el)
+}
 
+function SearchAll(el) {
+	return document.querySelectorAll(el)
+}
+
+// Табы из бонусного видео
+let modulesButton = SearchAll(".modules-buttons li"), // Берем кнопки
+	moduleBody = SearchAll(".module-body"); // Берем отдельные табы
+
+(function () {
+	for (let i = 0; i < moduleBody.length; i++) {
+		modulesButton[i].setAttribute("data-index", i); // кнопкам присваиваем дата-атрибут
+		moduleBody[i].setAttribute("data-index", i); // табам присваиваем дата-атрибут
+		modulesButton[0].classList.add("active"); // вешаем класс по умолчанию на первую кнопку
+		moduleBody[0].style.display = "block"; // ставим активным по умолчанию первый таб
+		modulesButton[i].onclick = switchmodule; // вешаем функцию переключения табов на кнопки
+	}
+})(); // Инициируем табы
+
+function switchmodule() {
+	for (let i = 0; i < moduleBody.length; i++) {
+		var a = this.getAttribute("data-index"); // берем в переменную значение дата-атрибута
+		moduleBody[i].style.display = "none"; // скрываем все табы
+		modulesButton[i].classList.remove("active"); // удаляем со всех кнопок класс
+		Search('.modules-buttons li[data-index="' + a + '"]').classList.add("active"); // вешаем класс на нажатую кнопку
+		Search('.module-body[data-index="' + a + '"]').style.display = "block"; // показываем соответсвующий таб
+	}
+}
 $(document).ready(function() {
         /* Якорь */
         $("a[href='#plan']").click(function (h) {
@@ -11,7 +41,61 @@ $(document).ready(function() {
             $("body,html").animate({
                 scrollTop: g
             }, 1500)
-		});
+        });
+        // can
+        $('.can-blocks').slick({
+            autoplay: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+            speed: 300,
+            arrows: false,
+            adaptiveHeight: true,
+            asNavFor: '.can-blocks-titles'
+            // centerPadding: '15px',
+            // adaptiveHeight: true,
+            // centerMode: true,
+            // responsive: [{
+            //     breakpoint: 1024,
+            //     settings: {
+            //         slidesToShow: 3,
+            //     }
+            // },
+            // {
+            //     breakpoint: 385,
+            //     settings: {
+            //         slidesToShow: 1
+            //     }
+            // }
+            // ]
+        });
+        $('.can-blocks-titles').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.can-blocks',
+            dots: false,
+            arrows: false,
+            // centerMode: true,
+            focusOnSelect: true,
+            infinite: true,
+            // initialSlide: '2'
+            // respondTo: '',
+            // fade: true
+                    responsive: [{
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+            ]
+          });
     $('[data-fancybox]').fancybox({
         loop: 1,
     });
